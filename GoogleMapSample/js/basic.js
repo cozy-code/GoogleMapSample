72ccf2940efe01049b5e8e190875da4e269d1ca1
cozy code <cozy.code@outlook.com>
2015-12-03 10:11:16 +0900

    add basic sample
    show basic map.

---
+++ GoogleMapSample/js/basic.js
@@ -0,0 +1,9 @@
+﻿// https://developers.google.com/maps/documentation/javascript/tutorial
+var map;
+function initMap() {
+    var tokyo= new google.maps.LatLng( 35.680674, 139.766773 );
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: tokyo
+    });
+}
